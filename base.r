d5bbb8f2fb1ca170f61041359ef926638bb8999d
unknown <barryr@NWC05BG41D1PRT.win.dowjones.net>
2015-06-17 14:04:48 -0400

    add base.r

---
+++ base.r
@@ -0,0 +1,11 @@
+# BASIC R FUNCTIONS - General purpose use
+
+# Automatically install packages and then require them.
+usePackage <- function(p) {
+  if (!is.element(p, installed.packages()[,1]))
+    install.packages(p, dep = TRUE)
+  require(p, character.only = TRUE)
+}
+
+# Clip dataframe to clipboard.
+ClipIt <- function(x) write.table(x, "clipboard-16384", sep="\t", row.names=F)
